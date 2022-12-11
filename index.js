dc42172bf5d2491d8bf1a69afc74f761e026fd7c
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-12-12 00:25:38 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+   // create an array from the input string
+   const wordArray = word.split("");
+   // reverse the array
+   const reversedWordArray = wordArray.reverse();
+   // create a string from the reversed array
+   const reversedWord = reversedWordArray.join("");
+   // return the reversed string
+   return reversedWord;
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
 
-/*
-  Add written explanation of your solution here
-*/
 
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
